66162cd4521c43b46f5e7e7eed1bdf8c7f895863
Giulio Piancastelli <giulio.piancastelli@gmail.com>
2017-12-01 16:41:27 +0100

    Day 1

---
+++ src/day1.clj
@@ -0,0 +1,31 @@
+(ns day1
+  (:require [clojure.string :as str]))
+
+(defn sum-next [seq]
+  (let [start [0, 0]
+        res (reduce
+          (fn [res, prev]
+            (let [partial (get res 0)]
+              [(if (== (get res 1) prev) (+ partial prev) partial), prev]))
+          start
+          (concat seq [(first seq)]))]
+    (get res 0)))
+
+(defn sum-halfway [seq]
+  (reduce
+    (fn [res, couple]
+      (let [first (get couple 0)
+            second (get couple 1)]
+        (if (== first second) (+ res first second) res)))
+    0
+    (let [whole (vec seq)
+          half (/ (count whole) 2)
+          first-half (subvec whole 0 half)
+          second-half (subvec whole half)]
+      (map vector first-half second-half))))
+
+(defn -main []
+  (let [text (str/trim (slurp "input/1.txt"))
+       seq (map #(Integer/parseInt %) (str/split text #""))]
+   (println (sum-next seq))
+   (println (sum-halfway seq))))
