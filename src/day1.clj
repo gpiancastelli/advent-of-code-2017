aa4cb26b57a44a9f42769713032e28bb92e62f6e
Giulio Piancastelli <giulio.piancastelli@gmail.com>
2017-12-02 17:19:37 +0100

    Pass sequence as vector

--- src/day1.clj
+++ src/day1.clj
@@ -2,13 +2,12 @@
   (:require [clojure.string :as str]))
 
 (defn sum-next [seq]
-  (let [start [0, 0]
-        res (reduce
+  (let [res (reduce
           (fn [res, prev]
             (let [partial (get res 0)]
               [(if (== (get res 1) prev) (+ partial prev) partial), prev]))
-          start
-          (concat seq [(first seq)]))]
+          [0, 0]
+          (conj seq (first seq)))]
     (get res 0)))
 
 (defn sum-halfway [seq]
@@ -18,14 +17,13 @@
             second (get couple 1)]
         (if (== first second) (+ res first second) res)))
     0
-    (let [whole (vec seq)
-          half (/ (count whole) 2)
-          first-half (subvec whole 0 half)
-          second-half (subvec whole half)]
+    (let [half-count (/ (count seq) 2)
+          first-half (subvec seq 0 half-count)
+          second-half (subvec seq half-count)]
       (map vector first-half second-half))))
 
 (defn -main []
   (let [text (str/trim (slurp "input/1.txt"))
-        seq (map #(Integer/parseInt %) (str/split text #""))]
+        seq (vec (map #(Integer/parseInt %) (str/split text #"")))]
     (println (sum-next seq))
     (println (sum-halfway seq))))
