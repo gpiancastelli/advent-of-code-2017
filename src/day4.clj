e87fabc2131282c6797b757d779cd93d3b59fc1b
Giulio Piancastelli <giulio.piancastelli@gmail.com>
2017-12-04 22:36:25 +0100

    Better names, better feature partition

--- src/day4.clj
+++ src/day4.clj
@@ -1,23 +1,19 @@
 (ns day4
   (:require [clojure.string :as str]))
 
-(defn no-duplicate-policy [chunked-phrase]
-  (if (= (count (set chunked-phrase))
-         (count chunked-phrase))
-    1
-    0))
+(defn has-no-duplicate [chunked-phrase]
+  (= (count (set chunked-phrase))
+     (count chunked-phrase)))
 
-(defn no-anagram-policy [chunked-phrase]
-  (if (= (count (set (map #(sort (str/split % #"")) chunked-phrase)))
-         (count chunked-phrase))
-    1
-    0))
+(defn has-no-anagram [chunked-phrase]
+  (= (count (set (map #(sort (str/split % #"")) chunked-phrase)))
+     (count chunked-phrase)))
 
 (defn count-valid [phrases policy]
   (let [chunked-phrases (map #(str/split % #"\s") phrases)]
-    (reduce + 0 (map policy chunked-phrases))))
+    (reduce + 0 (map #(if (policy %) 1 0) chunked-phrases))))
 
 (defn -main []
   (let [phrases (str/split (slurp "input/4.txt") #"\n")]
-    (println (count-valid phrases no-duplicate-policy))
-    (println (count-valid phrases no-anagram-policy))))
+    (println (count-valid phrases has-no-duplicate))
+    (println (count-valid phrases has-no-anagram))))
