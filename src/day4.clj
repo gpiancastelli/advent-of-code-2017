933d9a8bc6cab5fa274385abc9f892bd8fd926b8
Giulio Piancastelli <giulio.piancastelli@gmail.com>
2017-12-05 11:42:16 +0100

    reduce + map + 1 + 0 = count + filter

--- src/day4.clj
+++ src/day4.clj
@@ -11,7 +11,7 @@
 
 (defn count-valid [phrases policy]
   (let [chunked-phrases (map #(str/split % #"\s") phrases)]
-    (reduce + 0 (map #(if (policy %) 1 0) chunked-phrases))))
+    (count (filter policy chunked-phrases))))
 
 (defn -main []
   (let [phrases (str/split (slurp "input/4.txt") #"\n")]
