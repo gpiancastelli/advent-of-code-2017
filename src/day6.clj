7abbd31d3b6f53c4e805c82db45d774e879cfff7
Giulio Piancastelli <giulio.piancastelli@gmail.com>
2017-12-08 18:10:44 +0100

    Day 6

---
+++ src/day6.clj
@@ -0,0 +1,39 @@
+(ns day6
+  (:require [clojure.string :as str]))
+
+(defn find-max-blocks-index [banks]
+  (let [max-num-blocks (atom 0)
+        index (atom 0)]
+    (doseq [[i num-blocks] (map-indexed vector banks)]
+      (when (> num-blocks @max-num-blocks)
+        (reset! max-num-blocks num-blocks)
+        (reset! index i)))
+    @index))
+
+(defn redistribute [banks]
+  (let [index (atom (find-max-blocks-index banks))
+        num-blocks (get banks @index)
+        redistributed-banks (assoc banks @index 0)]
+    (loop [num-blocks num-blocks
+           banks redistributed-banks]
+      (swap! index inc)
+      (when (>= @index (count banks))
+        (reset! index 0))
+      (if (zero? num-blocks)
+        banks
+        (recur (dec num-blocks)
+               (assoc banks @index (inc (get banks @index))))))))
+
+(defn find-repetition [cycles configurations banks]
+  (let [configuration banks]
+    (if (contains? configurations configuration)
+      [cycles (get configurations configuration)]
+      (recur (inc cycles)
+             (assoc configurations configuration cycles)
+             (redistribute banks)))))
+
+(defn -main []
+  (let [banks (map #(Integer/parseInt %) (str/split (slurp "input/6.txt") #"\s"))
+        [total-cycles repeated-configuration-cycles] (find-repetition 0 {} (vec banks))]
+    (println total-cycles)
+    (println (- total-cycles repeated-configuration-cycles))))
